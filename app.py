399c2d4588b687eda651c658fbb86bb14f25c37e
sanjana kumari <sanjana1209kumari@gmail.com>
2025-10-31 12:51:02 +0530

    new changes

--- app.py
+++ app.py
@@ -3,8 +3,4 @@ def add_num(a,b):
     return a+b
 x=5
 y=3
-<<<<<<< HEAD
 print(f"the sum of {x} and {y} is {add_num(x,y)}")
-=======
-print(f"the sum of {x} and {y} is {add_num(x,y)}")
->>>>>>> 759b1cf712fe57ba4db06536efaa0bf86ff2e7a0
